2fc8cd387ce4e4bd3fa69f1e7522da56ddd45e7c
Piotr Kubas <piotrkubas39@gmail.com>
2025-01-07 12:17:12 +0100

    User interface changes

--- CodingTracker.PiotrrKubas/UserInterface.cs
+++ CodingTracker.PiotrrKubas/UserInterface.cs
@@ -1,17 +1,60 @@
 ﻿using Spectre.Console;
 using static CodingTracker.PiotrrKubas.Enums;
+
 namespace CodingTracker.PiotrrKubas
 {
     internal class UserInterface
     {
         internal void MainMenu()
         {
-            var rule = new Rule();
-            AnsiConsole.Write(rule);
+            while (true)
+            {
+                Console.Clear();
+                var rule = new Rule();
+                AnsiConsole.Write(rule);
 
-            AnsiConsole.Prompt(new SelectionPrompt<Selection>()
-                .Title("[yellow]MAIN MENU[/]")
-                .AddChoices(Enum.GetValues<Selection>()));
+                var actionChoice = AnsiConsole.Prompt
+                    (new SelectionPrompt<Selection>()
+                    .Title("[yellow]MAIN MENU[/]")
+                    .AddChoices(Enum.GetValues<Selection>()));
+
+                switch (actionChoice)
+                {
+                    case Selection.StartSession:
+                        StartSession();
+                        break;
+                    case Selection.EnterManually:
+                        ManualEntry();
+                        break;
+                    case Selection.Report:
+                        Report();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        
+        internal void StartSession()
+        {
+            Console.WriteLine("Session start");
+            DateTime sessionStart = DateTime.Now;
+            Console.WriteLine("Press any key to stop coding session");
+            Console.ReadKey();
+            DateTime sessionEnd = DateTime.Now;
+            TimeSpan duration = sessionEnd - sessionStart;
+            Console.WriteLine(duration);
+            Console.ReadKey();
+        }
+
+        internal void ManualEntry()
+        {
+            Console.WriteLine("Manual Entry");
+        }
+
+        internal void Report()
+        {
+            Console.WriteLine("Report printing");
         }
     }
 }
